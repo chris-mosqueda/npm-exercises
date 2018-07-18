0ecec79926ea01eba08743e584c5ba3a8e04ee51
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-07-18 14:00:16 -0500

    Updated js modules exercise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const $ = require('jquery');
 $("body").css("background-color", "green");
 
 let sayHello = myphrase => console.log(`Hello ${myphrase}`);
-sayHello("Newby");
+sayHello("Chris");
 
 const getMsg = require('./say-hello.js');
+
+import { square, diag } from './some-math.js';
+console.log(square(11)); // 121
+console.log(diag(4, 3)); // 5
+//OR
+// import * as lib from './say-hello';
+// console.log(lib.square(11)); // 121
+// console.log(lib.diag(4, 3)); // 5
--- src/say-hello.js
+++ src/say-hello.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const niceMsg = msg => {
-    return console.log("Good job, this worked!");
-};
+const niceMsg = msg => console.log("Good job, this worked!");
 
 module.exports = niceMsg();
